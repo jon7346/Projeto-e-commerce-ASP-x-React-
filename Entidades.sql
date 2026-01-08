331f33a89d6d67ca3930b414f7a865da05b8a4f9
Jonathan F. Felipe <jonathanfelipe7346@gmail.com>
2026-01-07 21:45:48 -0300

    Create Entidades.sql

---
+++ Entidades.sql
@@ -0,0 +1,58 @@
+/*
+Sistema de e-commercie 
+Backend em c# 
+front-end em react 
+banco em SQL 
+
+tarefas: 
+
+Definir entidades no banco de dados:
+produtos: 
+lojas: 
+clientes: 
+estoque: 
+carrinho: 
+itens do carrinho: 
+endereço: 
+
+
+construir a logica/fluxo de utlização do aplicativo 
+criar os controllers e classes 
+desenvolver a rest api ou as api 
+por fim o frontend em react 
+
+
+
+*/
+
+
+create database e_commercie; 
+go 
+use e_commercie;
+go
+
+create table produtos( 
+id int primary key, 
+preco decimal(13,2),
+nome varchar(150),
+marca varchar(150),
+descricao varchar(150),
+cor varchar(25),
+)
+
+create table lojas (
+id int primary key, 
+idEstoque int, 
+FOREIGN KEY (idEstoque) references estoque(id), 
+segmento varchar(150),
+nome varchar(150), 
+localidade varchar(150), 
+nota decimal(2,2)
+
+)
+
+create table estoque (
+id int primary key, 
+idProduto 
+idloja int, 
+foreign key      
